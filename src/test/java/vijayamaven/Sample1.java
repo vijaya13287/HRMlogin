62a1ceba3a8ea1c68391c08ed01decd0dad80c6a
Vijaya <Vijaya@Ananyasri>
2018-05-04 14:12:54 +0800

    Git success

---
+++ src/test/java/vijayamaven/Sample1.java
@@ -0,0 +1,18 @@
+package vijayamaven;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class Sample1 {
+
+	
+		
+		@Test
+		public static void testing() {
+			
+			FirefoxDriver Driver=new FirefoxDriver();
+			     Driver.get("https://www.google.com/?gws_rd=ssl");
+			     
+			
+} 
+}
